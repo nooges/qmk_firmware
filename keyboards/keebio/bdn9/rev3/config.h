c4b46fc9fea6e4ae8eefcb23ff0f40acf347c8ff
Danny Nguyen <danny@keeb.io>
2024-03-06 15:37:34 -0500

    Fix RGB

--- keyboards/keebio/bdn9/rev3/config.h
+++ keyboards/keebio/bdn9/rev3/config.h
@@ -6,11 +6,4 @@
 /* Defines for the RGB matrix */
 #ifdef RGB_MATRIX_ENABLE
 #   define NOP_FUDGE 0.4                // Chip/pin specific configs from this lone onward
-#   define WS2812_EXTERNAL_PULLUP
-#   define WS2812_PWM_DRIVER PWMD3
-#   define WS2812_PWM_CHANNEL 4
-#   define WS2812_PWM_PAL_MODE 10
-#   define WS2812_DMA_STREAM STM32_DMA1_STREAM2
-#   define WS2812_DMA_CHANNEL 2
-#   define WS2812_DMAMUX_ID STM32_DMAMUX1_TIM3_UP
 #endif
