3e43e1e36cf9f59a47dcb8cb64cb0c622db9a3c8
Danny Nguyen <danny@keeb.io>
2024-03-06 16:39:32 -0500

    Disable PD peripheral to re-enable B4 and B6

--- keyboards/keebio/bdn9/rev3/rev3.c
+++ keyboards/keebio/bdn9/rev3/rev3.c
@@ -3,6 +3,13 @@
 
 #include "quantum.h"
 
+void keyboard_pre_init_kb(void) {
+    // Disable the PD peripheral in pre-init because its pins are being used in the matrix:
+    PWR->CR3 |= PWR_CR3_UCPD_DBDIS;
+    // Call the corresponding _user() function (see https://docs.qmk.fm/#/custom_quantum_functions)
+    keyboard_pre_init_user();
+}
+
 #ifdef RGB_MATRIX_ENABLE
 led_config_t g_led_config = { {
     // Key Matrix to LED Index
@@ -39,30 +46,4 @@ void eeconfig_init_kb(void) {
 
     eeconfig_update_kb(0);
     eeconfig_init_user();
-}
-
-bool encoder_update_kb(uint8_t index, bool clockwise) {
-    if (!encoder_update_user(index, clockwise)) { return false; }
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    }
-    else if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_DOWN);
-        } else {
-            tap_code(KC_UP);
-        }
-    }
-    else if (index == 2) {
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
-        }
-    }
-    return false;
 }
